0b1a9b0179f6720c7d5abaa3f10eef1bc054aa58
Brooks Van Buren <woofiewilly@gmail.com>
2016-11-05 15:20:33 -0400

    initial commit

---
+++ config/config.js
@@ -0,0 +1,33 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'perscription-hasher'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/perscription-hasher-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'perscription-hasher'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/perscription-hasher-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+        name: 'express1'
+    },
+    port: process.env.port,
+  }
+};
+
+module.exports = config[env];
