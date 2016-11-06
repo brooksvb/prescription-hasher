56ba732aa9740a2a50d1959e11aceacccff1dafb
Brooks Van Buren <woofiewilly@gmail.com>
2016-11-06 04:51:38 -0500

    whoops

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,9 +3,6 @@ var express = require('express'),
   app = express(),
   Article = require('../models/article');
 
-var sha1 = require('../models/sha1');
-console.log(sha1);
-
 var bodyParser = require('body-parser');
 var multer = require('multer'); // v1.0.5
 var upload = multer(); // for parsing multipart/form-data
